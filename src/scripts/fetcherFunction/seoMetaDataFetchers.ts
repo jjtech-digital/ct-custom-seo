4e15d813a6e634b463ceab7c0e908c73688e57af
Shubham-compose-tech <shubham@compose.co.in>
2024-04-25 09:54:37 +0530

    feat: added new open ai key

--- src/scripts/fetcherFunction/seoMetaDataFetchers.ts
+++ src/scripts/fetcherFunction/seoMetaDataFetchers.ts
@@ -3,7 +3,7 @@ import OpenAI from 'openai';
 const apiKey =
   typeof process !== 'undefined' && process.env.OPENAI_API_KEY
     ? process.env.OPENAI_API_KEY
-    : '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';
+    : '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';
 const model =
   typeof process !== 'undefined' && process.env.OPENAI_MODEL
     ? process.env.OPENAI_MODEL
