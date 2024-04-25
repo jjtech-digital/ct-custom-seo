9bddfb90e542bbcef77ecccb848128f17ea7e475
Shubham-compose-tech <shubham@compose.co.in>
2024-04-25 11:46:24 +0530

    fix: open ai key issue

--- src/scripts/fetcherFunction/seoMetaDataFetchers.ts
+++ src/scripts/fetcherFunction/seoMetaDataFetchers.ts
@@ -1,23 +1,14 @@
 import OpenAI from 'openai';
 
-const apiKey =
-  typeof process !== 'undefined' && process.env.OPENAI_API_KEY
-    ? process.env.OPENAI_API_KEY
-    : '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';
-const model =
-  typeof process !== 'undefined' && process.env.OPENAI_MODEL
-    ? process.env.OPENAI_MODEL
-    : 'gpt-3.5-turbo';
-
 const openai = new OpenAI({
-  apiKey: apiKey,
+  apiKey: "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",
   dangerouslyAllowBrowser: true,
 });
 
 export const generateSeoMetaData = async (productName:string) => {
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: 'user', content: `Generate seo Title and seo Description for product ${productName}` }],
-    model: model,
+    model: "gpt-3.5-turbo",
   });
   return chatCompletion;
 };
